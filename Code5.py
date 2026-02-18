f8e39b5c987bd6d944f5dfea202eb1ef9b54ef1d
samanagaonkar <sama.nagaonkar2025@bitsdesign.edu.in>
2026-02-18 15:19:54 +0530

    sama_code_5.py

--- Code5.py
+++ Code5.py
@@ -1,1 +1,41 @@
+from machine import Pin
 
+import time
+
+in_list = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1 = Pin(14, Pin.OUT)
+in2 = Pin(25, Pin.OUT)
+in3 = Pin(26, Pin.OUT)
+in4 = Pin(27, Pin.OUT)
+
+pb1 = Pin(22, Pin.IN, Pin.PULL_UP)
+pb2 = Pin(18, Pin.IN, Pin.PULL_UP)
+
+count = 0
+
+while True:
+    button1 = pb1.value()
+    button2 = pb2.value()
+    
+    if button1 == 0 and button2 == 1:
+        while count < 501:
+            for i in in_list:
+                in1.value(i[0])
+                in2.value(i[1])
+                in3.value(i[2])
+                in4.value(i[3])
+                time.sleep(0.005)
+        
+            count = count + 4
+    
+    if button1 == 1 and button2 == 0:
+        while count < 501:
+            for i in reversed(in_list):
+                in1.value(i[0])
+                in2.value(i[1])
+                in3.value(i[2])
+                in4.value(i[3])
+                time.sleep(0.005)
+        
+            count = count + 4
